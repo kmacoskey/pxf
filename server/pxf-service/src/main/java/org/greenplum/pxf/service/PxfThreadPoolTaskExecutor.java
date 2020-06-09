e0969c19c33c467bbb304b93663d21620ce4dab2
Francisco Guerrero <aguerrero@pivotal.io>
2020-06-09 04:43:37 -0700

    Set pxf.conf property

--- server/pxf-service/src/main/java/org/greenplum/pxf/service/PxfThreadPoolTaskExecutor.java
+++ server/pxf-service/src/main/java/org/greenplum/pxf/service/PxfThreadPoolTaskExecutor.java
@@ -7,24 +7,22 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 
+import static org.greenplum.pxf.api.configuration.PxfServerProperties.PXF_CONF_PROPERTY;
+
 public class PxfThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
 
     private static final String PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_MESSAGE = "PXF Server processing capacity exceeded.";
-    private static final String PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_HINT = "Consider increasing the values of PXF_TASK_POOL_MAX_SIZE and/or PXF_TASK_POOL_QUEUE_CAPACITY in $PXF_CONF/conf/pxf-env.sh";
+    private static final String PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_HINT = "Consider increasing the values of PXF_TASK_POOL_MAX_SIZE and/or PXF_TASK_POOL_QUEUE_CAPACITY in '%s/conf/pxf-env.sh'";
 
     @Override
     public Future<?> submit(Runnable task) {
         try {
             return super.submit(task);
         } catch (TaskRejectedException ex) {
-
-            // TODO: set PXF_CONF as a system property
-
             PxfRuntimeException exception = new PxfRuntimeException(
                     PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_MESSAGE,
-                    /*String.format()*/ PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_HINT,
+                    String.format(PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_HINT, System.getProperty(PXF_CONF_PROPERTY)),
                     ex.getCause());
-
             throw new TaskRejectedException(ex.getMessage(), exception);
         }
     }
@@ -34,12 +32,10 @@ public class PxfThreadPoolTaskExecutor extends ThreadPoolTaskExecutor {
         try {
             return super.submit(task);
         } catch (TaskRejectedException ex) {
-
             PxfRuntimeException exception = new PxfRuntimeException(
                     PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_MESSAGE,
-                    /*String.format()*/ PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_HINT,
+                    String.format(PXF_SERVER_PROCESSING_CAPACITY_EXCEEDED_HINT, System.getProperty(PXF_CONF_PROPERTY)),
                     ex.getCause());
-
             throw new TaskRejectedException(ex.getMessage(), exception);
         }
     }
